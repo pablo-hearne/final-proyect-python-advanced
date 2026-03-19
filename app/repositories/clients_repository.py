5f6c86aa1a64edb067b44e59802262d41bf7ac66
pablo-hearne <pablohearne2@gmail.com>
2026-03-19 13:26:18 -0300

    started repositories and added the remote database

--- app/repositories/clients_repository.py
+++ app/repositories/clients_repository.py
@@ -1,15 +1,59 @@
 from fastapi import HTTPException
-from app.repositories.database import data
-from app.schemas.clients import Client
+from sqlalchemy.orm import Session, joinedload
+
+
+from app.repositories.models.clients_model import ClientsModel
+
+class Clients:
+
+
+    def get_clients(self,db : Session):
+        return db.query(ClientsModel).all()
+    
+
+    def get_client(self,db : Session,client_id:int):
+        client = db.query(ClientsModel).options(
+            joinedload(ClientsModel.pets),
+        ).filter_by(id=client_id).first()
+        if not client:
+            raise HTTPException(status_code=404, detail="Client not found")
+        return client
+    
+    def create_client(self, db : Session, client:ClientsModel):
+        new_client = ClientsModel(
+            name = client.name,
+            adress = client.adress,
+            number = client.number,
+            email = client.email
+        )
+        db.add(new_client)
+        db.commit()
+        db.refresh(new_client)
+        return new_client
+    
+    def update_client(self, db: Session, client_id : int, client : ClientsModel):
+        db_client = db.query(ClientsModel).filter_by(id=client_id).first()
+        if not db_client:
+            raise HTTPException(status_code=404, detail="Client not found")
+        if db_client:
+            db_client.name = client.name
+            db_client.adress = client.adress
+            db_client.email = client.email
+            db_client.number = client.number
+        db.commit()
+        db.refresh(db_client)
+        return db_client
+    
+    def delete_client(self, db : Session , client_id : int):
+        db_client = db.query(ClientsModel).filter_by(id=client_id).first()
+        # db_client = db.query(ClientsModel).filter_by(ClientsModel.id == client_id).first()   # Cuál es la diferencia entre ambos?
+        if db_client:
+            db.delete(db_client)
+            db.commit()
+            return db_client
+    
 
 
 
-class CLientsRepository:
-    def __init__(self) -> None:
-        self.data = data
-        pass
-    def get_clients(self):
-        return data
-    def get_client(self,client_id : int):
-        return self.data
-    pass
+
+
--- app/repositories/database.py
+++ app/repositories/database.py
@@ -1,13 +1,49 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# # URL de conexión a SQLite
+# DATABASE_URL = "sqlite:///./test.db"
+
+# # Crear motor de conexión a la base de datos
+# engine = create_engine(
+#     DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
+# # Crear una sesión para interactuar con la base de datos
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# # Base para definir los modelos de las tablas
+# Base = declarative_base()
+
+
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+
+"""Vamos a comparar los dos códigos para no perderme (uno con posgrade y otro con local)"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-# URL de conexión a SQLite
-DATABASE_URL = "sqlite:///./test.db"
+import os
+
+load_dotenv()
+
+# URL de conexión de db de Render
+DATABASE_URL = os.getenv("DATABASE_EXTERNAL_URL","sqlite:///./test.db")
 
 # Crear motor de conexión a la base de datos
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 # Crear una sesión para interactuar con la base de datos
@@ -24,8 +60,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-
-"""Vamos a comparar los dos códigos para no perderme (uno con posgrade y otro con local)"""
-
