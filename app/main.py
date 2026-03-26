15d00ae9143bb59d153f148bffdcdfc428a3a84a
pablo-hearne <pablohearne2@gmail.com>
2026-03-26 19:02:16 -0300

    Added so Vercel can access to database

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.controllers import clients_controllers,elements_controllers,pets_controllers,transactions_controllers,visits_controllers
 from app.repositories.database import Base,engine
+from fastapi.middleware.cors import CORSMiddleware
 # creo mi instancia de FastAPI
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["https://v0-veterinary-clinic-dashboard-two.vercel.app/"],
+    allow_credentials= True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+    )
 
 app.include_router(router=clients_controllers.router, prefix=f"/api/v1")
 app.include_router(router=pets_controllers.router, prefix=f"/api/v1")
